48055726aa31ed1209552bf00f3211ad982b4071
Cabra <greencoder001@outlook.com>
2026-05-05 20:06:51 +0200

    now creating zip for export

--- src/aio/export.rs
+++ src/aio/export.rs
@@ -1,5 +1,20 @@
-use std::path::PathBuf;
+use std::{fs::File, io::Write, path::PathBuf};
+
+use zip::{ZipWriter, write::FileOptions};
 
 pub fn export_system(filepath: &PathBuf) -> Result<(), String> {
+    let zipfile = File::create(&filepath).map_err(|e| e.to_string())?;
+    let mut zip = ZipWriter::new(zipfile);
+
+    let options: FileOptions<()> = FileOptions::default();
+
+    zip.start_file("readme.txt", options).map_err(|e| e.to_string())?;
+
+    zip.write_all(b"Hello, World!\n").map_err(|e| e.to_string())?;
+
+
+
+    zip.finish().map_err(|e| e.to_string())?;
+
     Ok(())
 }
