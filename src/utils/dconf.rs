c2fb6be412b2a1f71f22c2e99baa82edbb307d8b
Cabra <greencoder001@outlook.com>
2026-05-06 19:17:47 +0200

    included dconf export / dump

--- src/utils/dconf.rs
+++ src/utils/dconf.rs
@@ -1,8 +1,29 @@
-use std::fs::File;
+use std::{fs::File, io::Write, process::Command};
 
-use zip::ZipWriter;
+use colored::Colorize;
+use zip::{ZipWriter, write::FileOptions};
 
-// TODO: (via: dconf dump /org/)
 pub fn export_to_zip(zip: &mut ZipWriter<File>) -> Result<(), String> {
+    let out = Command::new("dconf").arg("dump").arg("/org/").output();
+
+    match out {
+        Ok(v) => {
+            if !v.status.success() {
+                return Err("Command 'dconf dump /org/' was not successful".into());
+            }
+
+            let out_str = String::from_utf8_lossy(&v.stdout);
+            let options: FileOptions<()> = FileOptions::default();
+
+            zip.start_file("dconf", options).map_err(|e| e.to_string())?;
+            zip.write_all(out_str.as_bytes()).map_err(|e| e.to_string())?;
+            zip.write_all(b"\n").map_err(|e| e.to_string())?;
+        }
+        Err(_) => {
+            println!("{}", "Warning: Skipping dconf export because it does not seem to be installed".yellow());
+            return Ok(());
+        }
+    }
+
     Ok(())
 }
